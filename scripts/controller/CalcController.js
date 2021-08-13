15182b5649558ae82eb3870cc689b89707a84003
Leonardo Henrique Guimaraes <leonardo.guimaraes20@icloud.com>
2021-08-13 15:45:31 -0300

    Creating initialize function

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,7 +1,14 @@
 class CalcController{
     constructor(){
+        this._locale = "pt-BR"
         this._displayCalculatorEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
         this._timeEl = document.querySelector("#hora")
+    }
+
+    initialize(){
+        setInterval(() => {
+            
+        }, 1000);
     }
 }
